09e766177d13bce07f8782addf4e744600ff389a
VEDANT9PAWASE <vedantpawase21@gmail.com>
2026-09-03 21:50:28 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+
+        int reverse = 0;
+
+        int length = String.valueOf(a).length();
+
+        for(int i = 0; i < length; i++) {
+            int digit = a % 10;
+            reverse = reverse * 10 + digit;
+            System.out.print(digit);
+            a = a / 10;
+        }
+        System.out.println("\nReversed number: " + reverse);
+        sc.close();
+    }
+}
+
