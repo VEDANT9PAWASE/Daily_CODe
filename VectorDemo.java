711d080546687738d0b46204d9ba1a7c8d139397
VEDANT9PAWASE <vedantpawase21@gmail.com>
2026-09-04 22:45:12 +0530

    SImple Java Vector Program

---
+++ VectorDemo.java
@@ -0,0 +1,12 @@
+
+import java.util.Vector;
+
+public class VectorDemo{
+    public static void main(String[] args){
+        Vector s=new Vector();
+
+        s.add("Nashik");
+        s.add("Mumbai");
+        System.out.print(s);
+    }
+}
